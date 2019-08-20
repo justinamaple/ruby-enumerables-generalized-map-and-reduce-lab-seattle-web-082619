b79832a7e0c0b92bb28c0e30ab075174b2d89c41
Justin Maple <justinamaple@gmail.com>
2019-08-20 05:45:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,7 +13,7 @@ def reduce(array, starting_value = 0)
     starting_value = yield(array[i], starting_value)
   end
   
-  if(starting_value.class == true.class || starting_value.class = false.class)
+  if(!!starting_value == starting_value)
     !!starting_value
   else
     starting_value
